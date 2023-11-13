c0c4c37a8620913896521bd73e2d04ab455f5340
abdulahad26i4 <abdulahad93500@gmail.com>
2023-11-13 19:56:41 +0500

    first commit

---
+++ type.js
@@ -0,0 +1,3 @@
+var Name = "Abdul Ahad";
+var message = "Hello Sir ".concat(Name, ", Thank You for Joining this Group.");
+console.log(message);
---
+++ type.ts
@@ -0,0 +1,3 @@
+var Name: string = "Abdul Ahad"
+var message: string = `Hello Sir ${Name}, Thank You for Joining this Group.`
+console.log(message) 
